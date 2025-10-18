b4405259298c4059cdc4d48ea6c00b2df7e2364d
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-18 22:11:52 +0530

    Initial commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,14 @@
-# backend/urls.py
-from django.urls import path, re_path
 from django.contrib import admin
-from accounts import views
-from .views import FrontendAppView
+from django.urls import path, re_path
+from django.views.generic import TemplateView
+from . import views  # or from your_app import views, depending on your structure
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
 
-    # Catch all other paths and serve React frontend
-    re_path(r'^.*$', FrontendAppView.as_view(), name='frontend'),
+    # Catch-all for React SPA
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import TemplateView
 from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
+
+    # Catch all other URLs and serve React app
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
