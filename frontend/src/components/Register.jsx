905d5e3774358e24745f313b21a8a7323f688e30
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-18 22:50:42 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./Register.css";
+import "./register.css";
 
 export default function Register() {
   const [formData, setFormData] = useState({
