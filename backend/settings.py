88e6f0fb5ef3a2b274e57209b8aaac262812988d
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-19 21:26:42 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -5,10 +5,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'your-secret-key'  # make sure this stays private
 DEBUG = True
 ALLOWED_HOSTS = [
-    "wdzinternship-mark2-of3l.onrender.com",  # your Render domain
-    "localhost",
-    "127.0.0.1",
+    'mini-lms-crh4.onrender.com',
+    'localhost', 
+    '127.0.0.1',
 ]
+
 
 # ------------------------------------------
 # 🔹 INSTALLED_APPS
