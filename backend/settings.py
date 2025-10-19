25d15affb5910c8d147886747140a7c8a3476271
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-19 22:12:34 +0530

    Change ROOT_URLCONF from backend.urls to accounts.urls

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'accounts.urls'
 
 # Template settings (adjust if needed)
 TEMPLATES = [
