8a4a773254761376872796d62d123750730f442c
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-21 22:01:48 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.views.generic import TemplateView
-from . import views  # or from your_app import views, depending on your structure
+from . import views # or from your_app import views (if this is the project-level urls.py)
 
 
 urlpatterns = [
+    # 1. DJANGO ADMIN
     path('admin/', admin.site.urls),
+    path('api/auth/', include('accounts.urls')), 
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
-
-    # Catch-all for React SPA
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
 ]
