9092457675c346d3c6b2d5a97327472354e0766e
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-23 06:36:33 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
-# accounts/urls.py
 from django.urls import path
-from . import views  # import your views here
+from . import views
 
 urlpatterns = [
     path('register/', views.register_user, name='register'),
     path('login/', views.login_user, name='login'),
-    path('profile/', views.profile, name='profile'),
+    # path('profile/', views.profile, name='profile'),  # Remove or comment this out
 ]
