58d00158d1a83662e0b9b68a145380d9524c4d9b
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-23 18:46:23 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('instructor', 'Instructor'),
+        ('admin', 'Admin'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+
     def __str__(self):
-        return self.username
+        return f"{self.username} ({self.role})"
