ba826c3a5607056fb2c9734e0ae5adf383af1dee
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-23 07:49:39 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),  # Include accounts app URLs here
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
