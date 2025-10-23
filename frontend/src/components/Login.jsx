b2c6206b79fc0924358cf6dfff97a7d28d9155f1
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-23 07:27:10 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://127.0.0.1:8000/login/", {
+      const response = await fetch("https://mini-lms-crh4.onrender.com/api/auth/login/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
