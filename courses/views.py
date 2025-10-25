37e32b055611374c17dde1949cdbfaf3335a0ea7
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-25 13:49:44 +0530

    Update views.py

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+# courses/views.py
 from rest_framework import generics, permissions
 from .models import Course
 from .serializers import CourseSerializer
@@ -6,8 +7,8 @@ class CourseListCreateView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
-    # anyone can view courses, but must be authenticated to create
     def get_permissions(self):
+        # anyone can view courses, but only authenticated users can create
         if self.request.method == "POST":
             return [permissions.IsAuthenticated()]
         return [permissions.AllowAny()]
@@ -19,12 +20,25 @@ class CourseListCreateView(generics.ListCreateAPIView):
 class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        # read-only allowed for anyone; updates/deletes require instructor or superuser
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
 
     def perform_update(self, serializer):
-        # ensure only instructor can edit their course
         course = self.get_object()
         if self.request.user == course.instructor or self.request.user.is_superuser:
             serializer.save()
         else:
-            raise permissions.PermissionDenied("You do not have permission to edit this course.")
+            from rest_framework.exceptions import PermissionDenied
+            raise PermissionDenied("You do not have permission to edit this course.")
+
+    def perform_destroy(self, instance):
+        course = instance
+        if self.request.user == course.instructor or self.request.user.is_superuser:
+            instance.delete()
+        else:
+            from rest_framework.exceptions import PermissionDenied
+            raise PermissionDenied("You do not have permission to delete this course.")
