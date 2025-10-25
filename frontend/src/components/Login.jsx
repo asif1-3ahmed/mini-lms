702bb90791ca733fd8e7337ea342ef72f4ea0315
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-25 13:55:28 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import API from "../api"; // use centralized axios instance
 import "./Login.css";
+import axios from "axios";
 
 export default function Login() {
   const [formData, setFormData] = useState({ username: "", password: "" });
