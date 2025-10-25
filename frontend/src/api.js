93ac970e6f6e534dff6291bbffeabb8b291df941
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-25 14:23:16 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = "https://mini-lms-crh4.onrender.com/api/auth";  // add /api/auth here
+import axios from "axios";
+
+const API_BASE_URL = "https://mini-lms-crh4.onrender.com/api/auth";
 
 const API = axios.create({
   baseURL: API_BASE_URL,
