eebeddd4533bd2171630caf5d9ef8313db843309
asif1-3ahmed <mdasifahmed07042006@gmail.com>
2025-10-25 13:50:00 +0530

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,8 @@
+# courses/urls.py
 from django.urls import path
 from .views import CourseListCreateView, CourseDetailView
 
 urlpatterns = [
-    path('', CourseListCreateView.as_view(), name='course_list_create'),
-    path('<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
+    path('courses/', CourseListCreateView.as_view(), name='course-list-create'),
+    path('courses/<int:pk>/', CourseDetailView.as_view(), name='course-detail'),
 ]
